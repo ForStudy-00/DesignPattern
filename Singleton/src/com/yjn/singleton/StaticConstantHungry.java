dd71a1b76c9d4c5cfb77f22ee749101e389420cd
yjn <977909144@qq.com>
2021-02-22 17:49:37 +0800

    饿汉式(静态变量)

--- Singleton/src/com/yjn/singleton/StaticConstantHungry.java
+++ Singleton/src/com/yjn/singleton/StaticConstantHungry.java
@@ -2,7 +2,6 @@ package com.yjn.singleton;
 
 /**
  * 静态常量饿汉式
- *
  * @author Lenovo
  */
 public class StaticConstantHungry {
@@ -28,3 +27,12 @@ class Singleton {
         return INSTANCE;
     }
 }
+/**
+ * 1）优点:写法简单 在类的装载过程就完成了实例化。避免了线程同步问题
+ * 2）缺点：在类的装载过程完成实例化，没有达到懒加载的效果，如果从始至终从没有使用过这个实例，就会造成内存浪费
+ * 3）这种方式基于classloader机制避免了多线程的同步问题，不过instance在类装载时就实例化，在单例模式中大多数都是调用
+ *    getInstance方法，但是导致类装载的原因有很多种，因此不能确定有其他方式（或者其他的静态方法）导致类加载，这时初始化
+ *    instance就没有达到懒加载的效果
+ * 4）结论：这种单例模式可用，可能会造成内存浪费。
+ *
+ **/
